814f89ef1619235d3fc994f8530a03b29fbb721f
narendra <narendraakula03@gmail.com>
2026-06-02 15:03:18 +0530

    Initial Terraform CI/CD project

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+    description = "AWS Region"
+    default = "ap-south-1"
+}
+variable "instance_type" {
+    description = "EC2 instance type"
+    default = "t2.micro"
+
+}
+
+variable "ami_id" {
+    description = "AMI ID"
+    default = "ami-0685bcc683dadb6b9"
